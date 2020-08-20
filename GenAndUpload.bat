acdfa0f17a6065811ef09f1eaad75a0243e76c37
Suresh Kumar Gadi <skgadi@users.noreply.github.com>
2020-08-20 03:16:59 -0500

    Generating and pushing into server.

--- GenAndUpload.bat
+++ GenAndUpload.bat
@@ -4,6 +4,7 @@ copy /y .\dev\*.min.* .\docs\
 md .\docs\css
 copy /y .\dev\css\*.min.css .\docs\css\
 
+ren .\docs\index.min.html index.html
 
 git add .
 git commit -am "Generating and pushing into server."
